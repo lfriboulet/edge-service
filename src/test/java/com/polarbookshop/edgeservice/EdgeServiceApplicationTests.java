3d384d2bc04c1bd0c57e71c1f7e5b083275f3cce
lfriboulet <laurent.friboulet@gmail.com>
2024-08-20 22:19:37 +0200

    fix error TU

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -1,13 +1,16 @@
 package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -18,6 +21,9 @@ class EdgeServiceApplicationTests {
 	@Container
 	static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.2"))
 			.withExposedPorts(REDIS_PORT);
+
+	@MockBean
+	ReactiveClientRegistrationRepository clientRegistrationRepository;
 
 	@DynamicPropertySource
 	static void redisProperties(DynamicPropertyRegistry registry) {
